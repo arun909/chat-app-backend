2e29769057b6fdc253a258ad212c7221b1889b41
arun909 <arunbabu7591@gmail.com>
2026-02-16 00:19:57 +0530

    implement login endpoint with JWT authentication and password verification

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,4 +1,4 @@
-const { registerUser } = require("./auth.service");
+const { registerUser, loginUser } = require("./auth.service");
 
 const register = async (req, res) => {
   try {
@@ -16,4 +16,21 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const userData = await loginUser(req.body);
+
+    res.status(200).json({
+      success: true,
+      data: userData
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+
+module.exports = { register, login };
